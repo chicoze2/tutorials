d8a20036f5ef8a9c44fe8021aeee3d3ca90d017d
Francisco José <chicoze247@gmail.com>
2023-07-21 01:30:01 -0300

    .

--- django-basics/src/products/views.py
+++ django-basics/src/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Product
 from .forms import ProductForm, RawProductForm
@@ -23,14 +23,12 @@ def product_create_view(request):
         Product.objects.create(**my_form.cleaned_data)
     else:
         print(my_form.errors)
-        
 
     context = {
         'form': my_form
     }
 
     return render(request, 'product/create.html', context)
-
 
 def product_detail_view(request):
 	obj = Product.objects.get(id=1)
@@ -40,3 +38,33 @@ def product_detail_view(request):
 
 	return render(request, 'product/detail.html', context)
     
+def dynamic_product_detail_view(request, my_id):
+    # obj = Product.objects.get(id=my_id)
+    if my_id:
+        obj = get_object_or_404(Product, id=my_id)
+
+        context = {
+             'object' : obj
+        }
+        return render(request, 'product/detail.html',context)
+    else:
+        #product list view
+        return redirect('products/')
+
+
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('/')
+    else:
+        context = {'object' : obj}
+        return render(request, 'product/delete.html',context)
+
+def product_list_view(request):
+    objs = Product.objects.all()
+    context = {'object_list' : objs}
+
+    return render(request, 'product/list.html', context)
--- django-basics/src/trydjango/urls.py
+++ django-basics/src/trydjango/urls.py
@@ -20,16 +20,19 @@ from django.urls import path
 
 #Own
 from pages.views import homepage_view, contact_view, about_view
-from products.views import product_detail_view, product_create_view
+from products.views import product_detail_view, product_create_view, dynamic_product_detail_view, product_delete_view, product_list_view
 
 urlpatterns = [
     path('', homepage_view, name='home'),
 
     path("contact/", contact_view),
     path("about/", about_view),
-    path("product/", product_detail_view),
-    path("create/", product_create_view),
 
+    #Refator to products app url pattern
+    path("product/<int:my_id>/", dynamic_product_detail_view, name='product_detail'),
+    path("create/", product_create_view),
+    path("product/<int:id>/delete/", product_delete_view),
+    path("products/", product_list_view),
 
     path('admin/', admin.site.urls),
 ]
